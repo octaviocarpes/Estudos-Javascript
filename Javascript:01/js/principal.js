ad9dd0daa9113f94d951478202d3c1f8a439d9b0
octaviocarpes <octavio.carpes10@gmail.com>
2017-12-01 10:27:18 -0200

    Inserido um laço para calcular o imce de todos os pacientes e quando tiver algum dado errado mudar a backgroud-color da linhada tabela para lightcoral.

--- Javascript:01/js/principal.js
+++ Javascript:01/js/principal.js
@@ -1,45 +1,42 @@
 var titulo = document.querySelector(".titulo");
 titulo.textContent = "Aparecida Nutricionista";
 
-var paciente = document.querySelector("#primeiro-paciente");
+var pacientes = document.querySelectorAll(".paciente");
 
-var tdPeso = paciente.querySelector(".info-peso");
-var tdAltura = paciente.querySelector(".info-altura");
+for (let index = 0; index < pacientes.length; index++) {
 
-let peso = tdPeso.textContent;
-var altura = tdAltura.textContent;
+    var paciente = pacientes[index];
 
-var tdImc = paciente.querySelector(".info-imc");
-tdImc.textContent = imc;
-
-var pesoEhValido = true;
-var alturaValida = true;
-
-if(peso <= 0 || peso > 1000){
-    console.log("Peso inválido");
-    tdImc.textContent = "Peso inválido!";
-    pesoEhValido = false;
-}
-
-
-if(altura <= 0 || altura >= 3.0){
-    tdImc.textContent = "Altura inválida!";
-    console.log("Altura inválida");
-    alturaValida = false;
-}
-
-if(pesoEhValido && alturaValida){
-    var imc = peso / (altura * altura);
+    var tdPeso = paciente.querySelector(".info-peso");
+    var tdAltura = paciente.querySelector(".info-altura");
+    
+    let peso = tdPeso.textContent;
+    var altura = tdAltura.textContent;
+    
+    var tdImc = paciente.querySelector(".info-imc");
     tdImc.textContent = imc;
-}
-
-console.log(peso);
-console.log(pesoEhValido);
-
-if(peso  < 0){
-    console.log(peso);
-}
-
-if(peso  > 1000){
-    console.log(peso);
+    
+    var pesoEhValido = true;
+    var alturaValida = true;
+    
+    if(peso <= 0 || peso >= 1000){
+        console.log("Peso inválido");
+        tdImc.textContent = "Peso inválido!";
+        pesoEhValido = false;
+        paciente.classList.add("paciente-invalido");
+    }
+    
+    
+    if(altura <= 0 || altura >= 3.0){
+        console.log("Altura inválida");
+        tdImc.textContent = "Altura inválida!";
+        alturaValida = false;
+        paciente.classList.add("paciente-invalido");
+    }
+    
+    if(pesoEhValido && alturaValida){
+        var imc = peso / (altura * altura);
+        tdImc.textContent = imc.toFixed(2);
+    }
+    
 }
