e5842c66176ec1051127fa8be0ccd86a5068b363
octaviocarpes <octavio.carpes10@gmail.com>
2017-12-01 08:49:43 -0200

    Adicionada função para validar o peso e a altura do paciente.

--- Javascript:01/js/principal.js
+++ Javascript:01/js/principal.js
@@ -6,10 +6,40 @@ var paciente = document.querySelector("#primeiro-paciente");
 var tdPeso = paciente.querySelector(".info-peso");
 var tdAltura = paciente.querySelector(".info-altura");
 
-var peso = tdPeso.textContent;
+let peso = tdPeso.textContent;
 var altura = tdAltura.textContent;
-
-var imc = peso / (altura * altura);
 
 var tdImc = paciente.querySelector(".info-imc");
 tdImc.textContent = imc;
+
+var pesoEhValido = true;
+var alturaValida = true;
+
+if(peso <= 0 || peso > 1000){
+    console.log("Peso inválido");
+    tdImc.textContent = "Peso inválido!";
+    pesoEhValido = false;
+}
+
+
+if(altura <= 0 || altura >= 3.0){
+    tdImc.textContent = "Altura inválida!";
+    console.log("Altura inválida");
+    alturaValida = false;
+}
+
+if(pesoEhValido && alturaValida){
+    var imc = peso / (altura * altura);
+    tdImc.textContent = imc;
+}
+
+console.log(peso);
+console.log(pesoEhValido);
+
+if(peso  < 0){
+    console.log(peso);
+}
+
+if(peso  > 1000){
+    console.log(peso);
+}
