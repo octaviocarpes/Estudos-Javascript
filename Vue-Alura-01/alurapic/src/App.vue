4f429930569bd48a1f43325216f2b80d0ce180c6
Octavio Carpes <octavio.carpes10@gmail.com>
2018-02-16 16:53:28 -0200

    Added filter feature

--- Vue-Alura-01/alurapic/src/App.vue
+++ Vue-Alura-01/alurapic/src/App.vue
@@ -1,19 +1,46 @@
 <template>
-  <div>
-    <h1>{{ titulo }}</h1>
-    <ul>
-      <li v-for="foto of fotos"><img :src="foto.url" :alt="foto.titulo"></li>
+  <div class="corpo">
+    <h1 class="centralizado">{{ titulo }}</h1>
+
+    <input type="search" class="filtro" v-on:input="filtro  = $event.target.value" placeholder="filtre por titulo">
+
+    <ul class="lista-fotos">
+
+      <li class="lista-fotos-item" v-for="foto of fotosComFiltro">
+
+        <meu-painel :titulo="foto.titulo">
+          <img class="imagem-responsiva" :src="foto.url" :alt="foto.titulo">
+        </meu-painel>
+
+      </li>
     </ul>
   </div>
 </template>
 
 <script>
+import Painel from './components/shared/painel/Painel.vue';
+  
   export default{
+
+  components:{
+    'meu-painel':Painel
+  },
 
     data(){
       return{
         titulo:'Alurapic',
-        fotos:[ ]
+        fotos:[ ],
+        filtro:''
+      }
+    },
+    computed:{
+      fotosComFiltro(){
+        if(this.filtro){
+          let exp = new RegExp(this.filtro.trim(), 'i');
+          return this.fotos.filter(foto => exp.test(foto.titulo));
+        }else{
+          return this.fotos;
+        }
       }
     },
     created(){
@@ -27,4 +54,30 @@
 
 <style>
 
+.centralizado {
+    text-align: center;
+  }
+
+  .corpo {
+    font-family: Helvetica, sans-serif;
+    margin: 0 auto;
+    width: 96%;
+  }
+
+  .lista-fotos {
+    list-style: none;
+  }
+
+  .lista-fotos .lista-fotos-item {
+    display: inline-block;
+  }
+
+.imagem-responsiva{
+  width: 100%;
+}
+
+.filtro{
+  display: block;
+  width: 100%;
+}
 </style>
