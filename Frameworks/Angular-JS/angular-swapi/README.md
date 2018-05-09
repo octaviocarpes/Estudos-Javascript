b7cb75b9a29d893d4344332a95beb5a62fd2afce
Octavio Carpes <octavio.carpes10@gmail.com>
2018-05-09 16:11:18 -0300

    Initial commit angular-swapi project

---
+++ Frameworks/Angular-JS/angular-swapi/README.md
@@ -0,0 +1,3 @@
+# Angular SWAPI!
+
+<p> This repo contains a little angular project that consumes the Star Wars API (SWAPI).</p>
---
+++ Frameworks/Angular-JS/angular-swapi/src/index.js
@@ -0,0 +1,6 @@
+var logToConsole = msg => {
+  console.log(msg);
+};
+
+let message = 'my message to log';
+logToConsole(message);
