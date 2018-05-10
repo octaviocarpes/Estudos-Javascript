cd468058728c0533406066d9bca5677113afb243
Octavio Carpes <octavio.carpes10@gmail.com>
2018-05-10 17:22:57 -0300

    Used prettier

--- Frameworks/Angular-JS/angular-swapi/src/index.js
+++ Frameworks/Angular-JS/angular-swapi/src/index.js
@@ -1,10 +1,10 @@
 let app = angular.module('main', ['ui.router']);
 
-app.config(function($stateProvider){
-    let homeState = {
-        name:'home',
-        url:'/',
-        template:'<h1>Home Page</h1>'
-    };
-    $stateProvider.state(homeState);
+app.config(function($stateProvider) {
+  let homeState = {
+    name: 'home',
+    url: '/',
+    template: '<h1>Home Page</h1>'
+  };
+  $stateProvider.state(homeState);
 });
