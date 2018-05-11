c53e4c159392346798c7dbcbb825d019829150cf
Octavio Carpes <octavio.carpes10@gmail.com>
2018-05-11 15:24:11 -0300

    hotfix:gulpfile and uirouter

--- Frameworks/Angular-JS/angular-swapi/src/index.js
+++ Frameworks/Angular-JS/angular-swapi/src/index.js
@@ -1,14 +1,17 @@
-angular.module('main',['ui.router']).config(['$stateProvider', function($stateProvider){
-  $stateProvider
-  .state({
-    url:'/hello',
-    name:'hello',
-    template:'Hello world!'
-  })
-  .state({
-    url:'/about',
-    name:'about',
-    template:'About'
-  })
-}]);
-console.log('FUUUUCK')
+angular.module('main', ['ui.router']).config([
+  '$stateProvider',
+  function($stateProvider) {
+    $stateProvider
+      .state({
+        url: '/hello',
+        name: 'hello',
+        template: 'Hello world!'
+      })
+      .state({
+        url: '/about',
+        name: 'about',
+        template: 'About'
+      });
+  }
+]);
+console.log('FUUUUCK');
